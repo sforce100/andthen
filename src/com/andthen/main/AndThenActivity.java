cd723460ae53466e5284eda88aa5e2e68ff09f38
unknown <Administrator@ZGC-20111120GES.(none)>
2013-04-01 21:17:30 +0800

    init project

---
+++ src/com/andthen/main/AndThenActivity.java
@@ -0,0 +1,13 @@
+package com.andthen.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndThenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
