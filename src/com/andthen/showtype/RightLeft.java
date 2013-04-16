e0ebef7c1aa9c0a67672f71e97c3a805f1439809
PlutoDyh <plutodyh26@163.com>
2013-04-17 00:27:25 +0800

    message

--- src/com/andthen/showtype/RightLeft.java
+++ src/com/andthen/showtype/RightLeft.java
@@ -30,11 +30,11 @@ public class RightLeft implements ShowType {
 			highlimit = true;
 
 		if (highlimit)
-			nexty += 4;
+			nexty += pery;
 		else
-			nexty -= 4;
+			nexty -= pery;
 
-		nextx -= 2;
+		nextx -= perx;
 
 		if (nextx < firstx-distance) {
 			state = 2;
@@ -55,11 +55,11 @@ public class RightLeft implements ShowType {
 			highlimit = true;
 
 		if (highlimit)
-			nexty += 4;
+			nexty += pery;
 		else
-			nexty -= 4;
+			nexty -= pery;
 
-		nextx += 2;
+		nextx += perx;
 
 		if (nextx > firstx) {
 			state = 4;
